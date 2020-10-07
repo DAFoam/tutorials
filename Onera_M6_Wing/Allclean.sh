a08b780d8ab219e2e7ea1794bf07dc17a79c44f4
Ping He <phe@iastate.edu>
2020-10-06 20:10:08 -0500

    Update.

--- Onera_M6_Wing/Allclean.sh
+++ Onera_M6_Wing/Allclean.sh
@@ -11,7 +11,7 @@ do
             rm -rf postProcessing
             rm -rf constant/extendedFeatureEdgeMesh
             rm -rf constant/triSurface
-            rm -rf constant/polyMesh/
+            rm -rf constant/polyMesh
             rm -rf *.bin *.info *.dat *.xyz *.stl
             rm -rf processor* 0.0000*
             rm -rf {1..9}*
