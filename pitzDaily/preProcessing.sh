decb1052f08eb91c0e8bf7f278572414682050e5
Ping He <phe@iastate.edu>
2021-02-13 15:13:13 -0600

    Added the pitzDaily topology optimization case.

---
+++ pitzDaily/preProcessing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if the OpenFOAM enviroments are loaded
+if [ -z "$WM_PROJECT" ]; then
+  echo "OpenFOAM environment not found, forgot to source the OpenFOAM bashrc?"
+  exit
+fi
+
+# generate mesh
+blockMesh
+
+# copy initial and boundary condition files
+cp -r 0.orig 0
