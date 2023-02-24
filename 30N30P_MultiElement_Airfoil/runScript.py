b4dd5c8107a053e4208bdcc4009f24c6e415b5ca
Ping He <phe@iastate.edu>
2023-02-23 20:14:26 -0600

    Added the option to print the opt problem at the beginning.

--- 30N30P_MultiElement_Airfoil/runScript.py
+++ 30N30P_MultiElement_Airfoil/runScript.py
@@ -354,7 +354,7 @@ else:
     exit(1)
 
 prob.driver.options["debug_print"] = ["nl_cons", "objs", "desvars"]
-# prob.driver.options["print_opt_prob"] = True
+prob.driver.options["print_opt_prob"] = True
 prob.driver.hist_file = "OptView.hst"
 
 if args.task == "opt":
