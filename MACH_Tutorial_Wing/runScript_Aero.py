b797463cf2b434465a4cf91581b5809add558be7
Ping He <phe@iastate.edu>
2023-02-23 20:14:26 -0600

    Added the option to print the opt problem at the beginning.

--- MACH_Tutorial_Wing/runScript_Aero.py
+++ MACH_Tutorial_Wing/runScript_Aero.py
@@ -249,7 +249,7 @@ else:
     exit(1)
 
 prob.driver.options["debug_print"] = ["nl_cons", "objs", "desvars"]
-# prob.driver.options["print_opt_prob"] = True
+prob.driver.options["print_opt_prob"] = True
 prob.driver.hist_file = "OptView.hst"
 
 
