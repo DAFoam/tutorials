53565460f5a57272d3624a4f5181e0d025fe44b5
Ping He <phe@iastate.edu>
2023-06-12 15:56:21 -0500

    Added the transonic setup for ADODG3.

---
+++ ADODG3_Wing/runScript_transonic_shape_planform.py
@@ -0,0 +1,350 @@
+#!/usr/bin/env python
+import os
+import argparse
+import numpy as np
+from mpi4py import MPI
+import openmdao.api as om
+from mphys.multipoint import Multipoint
+from dafoam.mphys import DAFoamBuilder, OptFuncs
+from mphys.scenario_aerodynamic import ScenarioAerodynamic
+from pygeo.mphys import OM_DVGEOCOMP
+from pygeo import geo_utils
+
+parser = argparse.ArgumentParser()
+# which optimizer to use. Options are: IPOPT (default), SLSQP, and SNOPT
+parser.add_argument("-optimizer", help="optimizer to use", type=str, default="IPOPT")
+# which task to run. Options are: opt (default), runPrimal, runAdjoint, checkTotals
+parser.add_argument("-task", help="type of run to do", type=str, default="opt")
+args = parser.parse_args()
+
+# =============================================================================
+# Input Parameters
+# =============================================================================
+
+U0 = 278
+p0 = 101325.0
+T0 = 300.0
+rho0 = p0 / T0 / 287.0
+nuTilda0 = 4.5e-5
+# Tu 0.5%, nu_r = 5
+k0 = 2.9
+epsilon0 = 1e4
+omega0 = 3.8e4
+CL_target = 0.5
+aoa0 = 2.0
+A0 = 3.0
+span0 = 3.0
+chord0 = 1.0
+
+daOptions = {
+    "designSurfaces": ["wing"],
+    "solverName": "DARhoSimpleCFoam",
+    "primalMinResTol": 1.0e-8,
+    "primalBC": {
+        "U0": {"variable": "U", "patches": ["inout"], "value": [U0, 0.0, 0.0]},
+        "p0": {"variable": "p", "patches": ["inout"], "value": [p0]},
+        "T0": {"variable": "T", "patches": ["inout"], "value": [T0]},
+        "nuTilda0": {"variable": "nuTilda", "patches": ["inout"], "value": [nuTilda0]},
+        "k0": {"variable": "k", "patches": ["inout"], "value": [k0]},
+        "epsilon0": {"variable": "epsilon", "patches": ["inout"], "value": [epsilon0]},
+        "omega0": {"variable": "omega", "patches": ["inout"], "value": [omega0]},
+        "useWallFunction": True,
+    },
+    "objFunc": {
+        "CD": {
+            "part1": {
+                "type": "force",
+                "source": "patchToFace",
+                "patches": ["wing"],
+                "directionMode": "parallelToFlow",
+                "alphaName": "aoa",
+                "scale": 1.0 / (0.5 * U0 * U0 * A0 * rho0),
+                "addToAdjoint": True,
+            }
+        },
+        "CL": {
+            "part1": {
+                "type": "force",
+                "source": "patchToFace",
+                "patches": ["wing"],
+                "directionMode": "normalToFlow",
+                "alphaName": "aoa",
+                "scale": 1.0 / (0.5 * U0 * U0 * A0 * rho0),
+                "addToAdjoint": True,
+            }
+        },
+    },
+    "adjStateOrdering": "cell",
+    "adjEqnOption": {
+        "gmresRelTol": 1.0e-6,
+        "pcFillLevel": 1,
+        "jacMatReOrdering": "natural",
+        "gmresMaxIters": 2000,
+        "gmresRestart": 2000,
+    },
+    # transonic preconditioner to speed up the adjoint convergence
+    "transonicPCOption": 1,
+    "normalizeStates": {
+        "U": U0,
+        "p": p0,
+        "T": T0,
+        "nuTilda": 1e-3,
+        "k": 1.0,
+        "omega": 100.0,
+        "epsilon": 10.0,
+        "phi": 1.0,
+    },
+    "designVar": {
+        "aoa": {"designVarType": "AOA", "patches": ["inout"], "flowAxis": "x", "normalAxis": "y"},
+        "twist": {"designVarType": "FFD"},
+        "shape": {"designVarType": "FFD"},
+    },
+}
+
+# Mesh deformation setup
+meshOptions = {
+    "gridFile": os.getcwd(),
+    "fileType": "OpenFOAM",
+    # point and normal for the symmetry plane
+    "symmetryPlanes": [[[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]]],
+}
+
+# Top class to setup the optimization problem
+class Top(Multipoint):
+    def setup(self):
+
+        # create the builder to initialize the DASolvers
+        dafoam_builder = DAFoamBuilder(daOptions, meshOptions, scenario="aerodynamic")
+        dafoam_builder.initialize(self.comm)
+
+        # add the design variable component to keep the top level design variables
+        self.add_subsystem("dvs", om.IndepVarComp(), promotes=["*"])
+
+        # add the mesh component
+        self.add_subsystem("mesh", dafoam_builder.get_mesh_coordinate_subsystem())
+
+        # add the geometry component (FFD)
+        self.add_subsystem("geometry", OM_DVGEOCOMP(file="FFD/wingFFD.xyz", type="ffd"))
+
+        # add a scenario (flow condition) for optimization, we pass the builder
+        # to the scenario to actually run the flow and adjoint
+        self.mphys_add_scenario("cruise", ScenarioAerodynamic(aero_builder=dafoam_builder))
+
+        # need to manually connect the x_aero0 between the mesh and geometry components
+        # here x_aero0 means the surface coordinates of structurally undeformed mesh
+        self.connect("mesh.x_aero0", "geometry.x_aero_in")
+        # need to manually connect the x_aero0 between the geometry component and the cruise
+        # scenario group
+        self.connect("geometry.x_aero0", "cruise.x_aero")
+
+    def configure(self):
+
+        # configure and setup perform a similar function, i.e., initialize the optimization.
+        # But configure will be run after setup
+
+        # add the objective function to the cruise scenario
+        self.cruise.aero_post.mphys_add_funcs()
+
+        # get the surface coordinates from the mesh component
+        points = self.mesh.mphys_get_surface_mesh()
+
+        # add pointset to the geometry component
+        self.geometry.nom_add_discipline_coords("aero", points)
+
+        # set the triangular points to the geometry component for geometric constraints
+        tri_points = self.mesh.mphys_get_triangulated_surface()
+        self.geometry.nom_setConstraintSurface(tri_points)
+
+        # Create reference axis for the twist variable
+        nRefAxPts = self.geometry.nom_addRefAxis(name="wingAxis", xFraction=0.25, alignIndex="k")
+
+        # Set up global design variables. We dont change the root twist
+        def twist(val, geo):
+            for i in range(1, nRefAxPts):
+                geo.rot_z["wingAxis"].coef[i] = -val[i - 1]
+
+        # define an angle of attack function to change the U direction at the far field
+        def aoa(val, DASolver):
+            aoa = val[0] * np.pi / 180.0
+            U = [float(U0 * np.cos(aoa)), float(U0 * np.sin(aoa)), 0.0]
+            # we need to update the U value only
+            DASolver.setOption("primalBC", {"U0": {"value": U}})
+            DASolver.updateDAOption()
+
+        # pass this aoa function to the cruise group
+        self.cruise.coupling.solver.add_dv_func("aoa", aoa)
+        self.cruise.aero_post.add_dv_func("aoa", aoa)
+
+        # add twist variable
+        self.geometry.nom_addGlobalDV(dvName="twist", value=np.array([0] * (nRefAxPts - 1)), func=twist)
+
+        # Set up the span variable, here val[0] is the span change in %
+        def span(val, geo):
+            # coordinates for the reference axis
+            refAxisCoef = geo.extractCoef("wingAxis")
+            # the relative location of a point in the ref axis
+            # refAxisS[0] = 0, and refAxisS[-1] = 1
+            refAxisS = geo.refAxis.curves[0].s
+            deltaSpan = span0 * val[0] / 100.0
+            # linearly change the refAxis coef along the span
+            for i in range(nRefAxPts):
+                refAxisCoef[i, 2] += refAxisS[i] * deltaSpan
+            geo.restoreCoef(refAxisCoef, "wingAxis")
+
+        # add span variable
+        self.geometry.nom_addGlobalDV(dvName="span", value=np.array([0]), func=span)
+
+        # Set up the taper variable, val[0] is the chord change in % at the root and
+        # val[1] is the chord change at the tip, the chords at other spanwise locations
+        # will be linearly determined by the root and tip chords
+        def taper(val, geo):
+            refAxisS = geo.refAxis.curves[0].s
+            cRoot = chord0 * val[0] / 100.0
+            cTip = chord0 * val[1] / 100.0
+            for i in range(nRefAxPts):
+                geo.scale_x["wingAxis"].coef[i] = 1.0 + refAxisS[i] * (cTip - cRoot) + cRoot
+
+        # add taper variable
+        self.geometry.nom_addGlobalDV(dvName="taper", value=np.array([0, 0]), func=taper)
+
+        # Set up the sweep variable, here val[0] is the sweep change degree
+        def sweep(val, geo):
+            refAxisCoef = geo.extractCoef("wingAxis")
+            refAxisS = geo.refAxis.curves[0].s
+            sweepDeg = np.deg2rad(float(val[0]))
+            xDelta = np.tan(sweepDeg) * span0
+            for i in range(len(refAxisCoef)):
+                refAxisCoef[i, 0] = refAxisCoef[i, 0] + refAxisS[i] * xDelta
+            geo.restoreCoef(refAxisCoef, "wingAxis")
+
+        # add span variable
+        self.geometry.nom_addGlobalDV(dvName="sweep", value=np.array([0]), func=sweep)
+
+        # select the FFD points to move
+        pts = self.geometry.DVGeo.getLocalIndex(0)
+        indexList = pts[:, :, :].flatten()
+        PS = geo_utils.PointSelect("list", indexList)
+        nShapes = self.geometry.nom_addLocalDV(dvName="shape", pointSelect=PS)
+
+        # setup the volume and thickness constraints
+        leList = [[0.02, 0.0, 1e-3], [0.02, 0.0, 2.9]]
+        teList = [[0.95, 0.0, 1e-3], [0.95, 0.0, 2.9]]
+        self.geometry.nom_addThicknessConstraints2D("thickcon", leList, teList, nSpan=25, nChord=30)
+        self.geometry.nom_addVolumeConstraint("volcon", leList, teList, nSpan=25, nChord=30)
+        # add the LE/TE constraints
+        self.geometry.nom_add_LETEConstraint("lecon", volID=0, faceID="iLow")
+        self.geometry.nom_add_LETEConstraint("tecon", volID=0, faceID="iHigh")
+
+        # add the design variables to the dvs component's output
+        self.dvs.add_output("twist", val=np.array([0] * (nRefAxPts - 1)))
+        self.dvs.add_output("span", val=np.array([0]))
+        self.dvs.add_output("taper", val=np.array([0, 0]))
+        self.dvs.add_output("sweep", val=np.array([0]))
+        self.dvs.add_output("shape", val=np.array([0] * nShapes))
+        self.dvs.add_output("aoa", val=np.array([aoa0]))
+        # manually connect the dvs output to the geometry and cruise
+        self.connect("twist", "geometry.twist")
+        self.connect("span", "geometry.span")
+        self.connect("taper", "geometry.taper")
+        self.connect("sweep", "geometry.sweep")
+        self.connect("shape", "geometry.shape")
+        self.connect("aoa", "cruise.aoa")
+
+        # define the design variables
+        self.add_design_var("twist", lower=-10.0, upper=10.0, scaler=0.1)
+        self.add_design_var("span", lower=-30.0, upper=30.0, scaler=0.01)
+        self.add_design_var("taper", lower=[0.0, -30.0], upper=30.0, scaler=0.01)
+        self.add_design_var("sweep", lower=0.0, upper=30.0, scaler=0.01)
+        self.add_design_var("shape", lower=-1.0, upper=1.0, scaler=10.0)
+        self.add_design_var("aoa", lower=0.0, upper=10.0, scaler=1.0)
+
+        # add objective and constraints to the top level
+        self.add_objective("cruise.aero_post.CD", scaler=1.0)
+        self.add_constraint("cruise.aero_post.CL", equals=CL_target, scaler=1.0)
+        self.add_constraint("geometry.thickcon", lower=0.5, upper=3.0, scaler=1.0)
+        self.add_constraint("geometry.volcon", lower=1.0, scaler=1.0)
+        self.add_constraint("geometry.tecon", equals=0.0, scaler=1.0, linear=True)
+        self.add_constraint("geometry.lecon", equals=0.0, scaler=1.0, linear=True)
+
+
+# OpenMDAO setup
+prob = om.Problem()
+prob.model = Top()
+prob.setup(mode="rev")
+om.n2(prob, show_browser=False, outfile="mphys.html")
+
+# initialize the optimization function
+optFuncs = OptFuncs(daOptions, prob)
+
+# use pyoptsparse to setup optimization
+prob.driver = om.pyOptSparseDriver()
+prob.driver.options["optimizer"] = args.optimizer
+# options for optimizers
+if args.optimizer == "SNOPT":
+    prob.driver.opt_settings = {
+        "Major feasibility tolerance": 1.0e-5,
+        "Major optimality tolerance": 1.0e-5,
+        "Minor feasibility tolerance": 1.0e-5,
+        "Verify level": -1,
+        "Function precision": 1.0e-5,
+        "Major iterations limit": 100,
+        "Nonderivative linesearch": None,
+        "Print file": "opt_SNOPT_print.txt",
+        "Summary file": "opt_SNOPT_summary.txt",
+    }
+elif args.optimizer == "IPOPT":
+    prob.driver.opt_settings = {
+        "tol": 1.0e-5,
+        "constr_viol_tol": 1.0e-5,
+        "max_iter": 100,
+        "print_level": 5,
+        "output_file": "opt_IPOPT.txt",
+        "mu_strategy": "adaptive",
+        "limited_memory_max_history": 10,
+        "nlp_scaling_method": "none",
+        "alpha_for_y": "full",
+        "recalc_y": "yes",
+    }
+elif args.optimizer == "SLSQP":
+    prob.driver.opt_settings = {
+        "ACC": 1.0e-5,
+        "MAXIT": 100,
+        "IFILE": "opt_SLSQP.txt",
+    }
+else:
+    print("optimizer arg not valid!")
+    exit(1)
+
+prob.driver.options["debug_print"] = ["nl_cons", "objs", "desvars"]
+prob.driver.options["print_opt_prob"] = True
+prob.driver.hist_file = "OptView.hst"
+
+
+if args.task == "opt":
+    # solve CL
+    optFuncs.findFeasibleDesign(["cruise.aero_post.CL"], ["aoa"], targets=[CL_target])
+    # run the optimization
+    prob.run_driver()
+elif args.task == "runPrimal":
+    # just run the primal once
+    prob.run_model()
+elif args.task == "runAdjoint":
+    # just run the primal and adjoint once
+    prob.run_model()
+    totals = prob.compute_totals()
+    if MPI.COMM_WORLD.rank == 0:
+        print(totals)
+elif args.task == "checkTotals":
+    # verify the total derivatives against the finite-difference
+    prob.run_model()
+    prob.check_totals(
+        of=["cruise.aero_post.CD", "cruise.aero_post.CL"],
+        wrt=["shape", "aoa"],
+        compact_print=True,
+        step=1e-3,
+        form="central",
+        step_calc="abs",
+    )
+else:
+    print("task arg not found!")
+    exit(1)
