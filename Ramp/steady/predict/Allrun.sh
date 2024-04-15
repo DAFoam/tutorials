5420fb48d132f2909fedc155f3347dc0bc965b83
Ping He <phe@iastate.edu>
2024-04-14 20:30:12 -0500

    Fixed test.

--- Ramp/steady/predict/Allrun.sh
+++ Ramp/steady/predict/Allrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd baseline && mpirun -np 4 python runScript.py && cd ..
-cd reference && mpirun -np 4 python runScript.py && cd ..
-cd trained && mpirun -np 4 python runScript.py && cd ..
+cd baseline && mpirun --oversubscribe -np 4 python runScript.py && cd ..
+cd reference && mpirun --oversubscribe -np 4 python runScript.py && cd ..
+cd trained && mpirun --oversubscribe -np 4 python runScript.py && cd ..
 
 
