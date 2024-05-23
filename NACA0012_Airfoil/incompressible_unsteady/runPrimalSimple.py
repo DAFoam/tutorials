835d7f4fbe89f37976f12a26adbe0ab61c071b10
Ping He <phe@iastate.edu>
2024-05-22 20:54:53 -0500

    Added the unsteady airfoil case.

---
+++ NACA0012_Airfoil/incompressible_unsteady/runPrimalSimple.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+"""
+DAFoam run script for the NACA0012 airfoil at low-speed (unsteady)
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+from mpi4py import MPI
+from dafoam import PYDAFOAM, optFuncs
+import numpy as np
+
+# =============================================================================
+# Input Parameters
+# =============================================================================
+gcomm = MPI.COMM_WORLD
+
+U0 = 10.0
+A0 = 0.1
+aoa0 = 15.0
+
+# Set the parameters for optimization
+daOptions = {
+    "designSurfaces": ["wing"],
+    "solverName": "DASimpleFoam",
+    "primalMinResTol": 1.0e-16,
+    "primalMinResTolDiff": 1.0e16,
+    "primalBC": {
+        "U0": {"variable": "U", "patches": ["inout"], "value": [U0, 0.0, 0.0]},
+        "useWallFunction": True,
+    },
+    "objFunc": {
+        "CD": {
+            "part1": {
+                "type": "force",
+                "source": "patchToFace",
+                "patches": ["wing"],
+                "directionMode": "parallelToFlow",
+                "alphaName": "aoa",
+                "scale": 1.0 / (0.5 * U0 * U0 * A0),
+                "addToAdjoint": True,
+            }
+        },
+        "CL": {
+            "part1": {
+                "type": "force",
+                "source": "patchToFace",
+                "patches": ["wing"],
+                "directionMode": "normalToFlow",
+                "alphaName": "aoa",
+                "scale": 1.0 / (0.5 * U0 * U0 * A0),
+                "addToAdjoint": True,
+            }
+        },
+    },
+    "designVar": {
+        "aoa": {"designVarType": "AOA", "patches": ["inout"], "flowAxis": "x", "normalAxis": "y"}
+    },
+}
+
+
+def aoa(val, geo):
+    aoa = val[0] * np.pi / 180.0
+    inletU = [float(U0 * np.cos(aoa)), float(U0 * np.sin(aoa)), 0]
+    DASolver.setOption("primalBC", {"U0": {"variable": "U", "patches": ["inout"], "value": inletU}})
+    DASolver.updateDAOption()
+
+
+DASolver = PYDAFOAM(options=daOptions, comm=gcomm)
+DASolver.addInternalDV("aoa", [aoa0], aoa, lower=-50, upper=50, scale=1.0)
+
+optFuncs.DASolver = DASolver
+optFuncs.DVGeo = None
+optFuncs.DVCon = None
+optFuncs.evalFuncs = ["CD", "CL"]
+optFuncs.gcomm = gcomm
+
+optFuncs.runPrimal()
