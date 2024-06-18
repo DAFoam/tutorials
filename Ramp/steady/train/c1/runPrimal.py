bcda085e87130bb78f3bce06d0ecdf23c0dd0242
Ping He <phe@iastate.edu>
2024-06-18 14:39:16 -0500

    Updated ramp scripts.

--- Ramp/steady/train/c1/runPrimal.py
+++ Ramp/steady/train/c1/runPrimal.py
@@ -4,7 +4,12 @@
 # Imports
 # =============================================================================
 from mpi4py import MPI
-from dafoam import PYDAFOAM, optFuncs
+from dafoam import PYDAFOAM
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-augmented", help="whether to use augumented model", type=bool, default=False)
+args = parser.parse_args()
 
 gcomm = MPI.COMM_WORLD
 
@@ -13,7 +18,37 @@ U0 = 10.0
 # Set the parameters for optimization
 daOptions = {
     "solverName": "DASimpleFoam",
+    "primalMinResTol": 1.0e-8,
+    "primalMinResTolDiff": 1e8,
     "primalBC": {"U0": {"variable": "U", "patches": ["inlet"], "value": [U0, 0, 0]}, "useWallFunction": True},
+    "primalVarBounds": {"omegaMin": -1e16},
+    "regressionModel": {
+        "active": args.augmented,
+        "model": {
+            "writeFeatures": True,
+            "modelType": "externalTensorFlow",
+            "inputNames": ["PoD", "VoS", "PSoSS", "KoU2"],
+            "outputName": "betaFIOmega",
+            "hiddenLayerNeurons": [20, 20],
+            "inputShift": [0.0, 0.0, 0.0, 0.0],
+            "inputScale": [1.0, 1.0, 1.0, 0.1],
+            "outputShift": 1.0,
+            "outputScale": 1.0,
+            "activationFunction": "tanh",
+            "printInputInfo": True,
+            "defaultOutputValue": 1.0,
+            "outputUpperBound": 1e1,
+            "outputLowerBound": -1e1,
+            "writeFeatures": True,
+        }
+    },
+    "tensorflow": {
+        "active": args.augmented,
+        "model": {
+            "predictBatchSize": 10000,
+            "nInputs": 4,
+        },
+    },
     "objFunc": {
         "CD": {
             "part1": {
@@ -23,7 +58,18 @@ daOptions = {
                 "directionMode": "fixedDirection",
                 "direction": [1.0, 0.0, 0.0],
                 "scale": 1.0,
-                "addToAdjoint": True,
+                "addToAdjoint": False,
+            }
+        },
+        "CL": {
+            "part1": {
+                "type": "force",
+                "source": "patchToFace",
+                "patches": ["bot"],
+                "directionMode": "fixedDirection",
+                "direction": [0.0, 1.0, 0.0],
+                "scale": 1.0,
+                "addToAdjoint": False,
             }
         },
     },
