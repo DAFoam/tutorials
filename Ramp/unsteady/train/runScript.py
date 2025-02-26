e3846371ee4b4edcd6f84d224a82f12f3fac9ea6
Ping He <phe@iastate.edu>
2025-02-26 16:34:54 -0600

    Changed the unsteady FIML script.

--- Ramp/unsteady/train/runScript.py
+++ Ramp/unsteady/train/runScript.py
@@ -4,49 +4,49 @@
 # Imports
 # =============================================================================
 import argparse
-from mpi4py import MPI
-from dafoam import PYDAFOAM, optFuncs
-from pyoptsparse import Optimization, OPT
 import numpy as np
-import json
+from mpi4py import MPI
+import openmdao.api as om
+from mphys.multipoint import Multipoint
+from dafoam.mphys import DAFoamBuilder
+from mphys.scenario_aerodynamic import ScenarioAerodynamic
+
 np.set_printoptions(precision=8, threshold=10000)
 
 # =============================================================================
 # Input Parameters
 # =============================================================================
 parser = argparse.ArgumentParser()
-parser.add_argument("--task", help="type of run to do", type=str, default="opt")
-parser.add_argument("--optimizer", help="optimizer to use", type=str, default="snopt")
-parser.add_argument("--readDV", help="whether to read designVariable.json", type=int, default=0)
-parser.add_argument("--mode", help="AD mode: either reverse or forward", type=str, default="reverse")
-parser.add_argument("--seedIndex", help="which design variable index to set seeds", type=int, default=0)
+# which optimizer to use. Options are: IPOPT (default), SLSQP, and SNOPT
+parser.add_argument("-optimizer", help="optimizer to use", type=str, default="IPOPT")
+# which task to run. Options are: run_driver (default), run_model, compute_totals, check_totals
+parser.add_argument("-task", help="type of run to do", type=str, default="run_driver")
+# which case to run
+parser.add_argument("-index", help="which case index to run", type=int, default=0)
 args = parser.parse_args()
-gcomm = MPI.COMM_WORLD
 
 np.random.seed(0)
 
 # Set the parameters for optimization
 daOptions = {
     "solverName": "DAPimpleFoam",
-    "useAD": {"mode": args.mode},
     "unsteadyAdjoint": {
         "mode": "timeAccurate",
         "PCMatPrecomputeInterval": 50,
         "PCMatUpdateInterval": 1,
-        "objFuncTimeOperator": "average",
         "reduceIO": True,
     },
     "primalBC": {"useWallFunction": False},
     "printIntervalUnsteady": 1,
     "regressionModel": {
         "active": True,
-        "model1": {
+        "reg_model1": {
             "modelType": "neuralNetwork",
             "inputNames": ["PoD", "VoS", "chiSA", "PSoSS"],
             "outputName": "betaFINuTilda",
             "hiddenLayerNeurons": [20, 20],
             "inputShift": [0.0, 0.0, 0.0, 0.0],
-            "inputScale": [0.001, 1.0, 0.01, 1.0],
+            "inputScale": [1.0, 1.0, 1.0, 1.0],
             "outputShift": 1.0,
             "outputScale": 1.0,
             "activationFunction": "tanh",
@@ -54,36 +54,29 @@ daOptions = {
             "defaultOutputValue": 1.0,
             "outputUpperBound": 1e1,
             "outputLowerBound": -1e1,
-        }
+        },
     },
-    "objFunc": {
-        "VAR": {
-            "pVar": {
-                "type": "variance",
-                "source": "boxToCell",
-                "min": [-10.0, -10.0, -10.0],
-                "max": [10.0, 10.0, 10.0],
-                "scale": 0.02,
-                "mode": "surface",
-                "surfaceNames": ["bot"],
-                "varName": "p",
-                "varType": "scalar",
-                "timeDependentRefData": True,
-                "addToAdjoint": True,
-            },
-            "betaVar": {
-                "type": "variance",
-                "source": "boxToCell",
-                "min": [-10.0, -10.0, -10.0],
-                "max": [10.0, 10.0, 10.0],
-                "scale": 0.01,
-                "mode": "field",
-                "varName": "betaFINuTilda",
-                "varType": "scalar",
-                "timeOperator": "average",
-                "timeDependentRefData": False,
-                "addToAdjoint": True,
-            },
+    "function": {
+        "pVar": {
+            "type": "variance",
+            "source": "patchToFace",
+            "patches": ["bot"],
+            "scale": 0.02,
+            "mode": "surface",
+            "varName": "p",
+            "varType": "scalar",
+            "timeDependentRefData": True,
+            "timeOp": "average",
+        },
+        "betaVar": {
+            "type": "variance",
+            "source": "allCells",
+            "scale": 0.01,
+            "mode": "field",
+            "varName": "betaFINuTilda",
+            "varType": "scalar",
+            "timeDependentRefData": False,
+            "timeOp": "average",
         },
     },
     "adjStateOrdering": "cell",
@@ -100,14 +93,53 @@ daOptions = {
         "nuTilda": 1e-3,
         "phi": 1.0,
     },
-    "designVar": {
-        "parameter": {"designVarType": "RegPar", "modelName": "model1"},
+    "inputInfo": {
+        "reg_model1": {"type": "regressionPar", "components": ["solver", "function"]},
     },
 }
 
+
+class Top(Multipoint):
+    def setup(self):
+
+        dafoam_builder = DAFoamBuilder(daOptions, None, scenario="aerodynamic")
+        dafoam_builder.initialize(self.comm)
+
+        # ivc to keep the top level DVs
+        self.add_subsystem("dvs", om.IndepVarComp(), promotes=["*"])
+
+        self.mphys_add_scenario("scenario1", ScenarioAerodynamic(aero_builder=dafoam_builder))
+
+        # setup a composite objective
+        self.add_subsystem("obj", om.ExecComp("val=error+regulation"))
+
+    def configure(self):
+
+        nParameters = self.scenario1.coupling.solver.DASolver.getNRegressionParameters("reg_model1")
+        parameter0 = (np.random.rand(nParameters) - 0.5) * 0.05
+        self.dvs.add_output("reg_model1", val=parameter0)
+        self.connect("reg_model1", "scenario1.reg_model1")
+
+        # define the design variables to the top level
+        self.add_design_var("reg_model1", lower=-100.0, upper=100.0, scaler=1.0)
+        # add the objective
+        self.connect("scenario1.aero_post.pVar", "obj.error")
+        self.connect("scenario1.aero_post.betaVar", "obj.regulation")
+        self.add_objective("obj.val", scaler=1.0)
+
+
+# OpenMDAO setup
+prob = om.Problem()
+prob.model = Top()
+prob.setup(mode="rev")
+om.n2(prob, show_browser=False, outfile="mphys.html")
+
+# use pyoptsparse to setup optimization
+prob.driver = om.pyOptSparseDriver()
+prob.driver.options["optimizer"] = args.optimizer
 # options for optimizers
-if args.optimizer == "snopt":
-    optOptions = {
+if args.optimizer == "SNOPT":
+    prob.driver.opt_settings = {
         "Major feasibility tolerance": 1.0e-6,
         "Major optimality tolerance": 1.0e-6,
         "Minor feasibility tolerance": 1.0e-6,
@@ -120,96 +152,49 @@ if args.optimizer == "snopt":
         "Print file": "opt_SNOPT_print.txt",
         "Summary file": "opt_SNOPT_summary.txt",
     }
-elif args.optimizer == "ipopt":
-    optOptions = {
-        "tol": 1.0e-6,
-        "constr_viol_tol": 1.0e-6,
-        "max_iter": 200,
+elif args.optimizer == "IPOPT":
+    prob.driver.opt_settings = {
+        "tol": 1.0e-5,
+        "constr_viol_tol": 1.0e-5,
+        "max_iter": 50,
         "print_level": 5,
         "output_file": "opt_IPOPT.txt",
         "mu_strategy": "adaptive",
-        "limited_memory_max_history": 100,
+        "limited_memory_max_history": 10,
         "nlp_scaling_method": "none",
         "alpha_for_y": "full",
         "recalc_y": "yes",
     }
-elif args.optimizer == "slsqp":
-    optOptions = {
-        "ACC": 1.0e-7,
-        "MAXIT": 200,
+elif args.optimizer == "SLSQP":
+    prob.driver.opt_settings = {
+        "ACC": 1.0e-5,
+        "MAXIT": 100,
         "IFILE": "opt_SLSQP.txt",
     }
 else:
-    print("opt arg not valid!")
-    exit(0)
+    print("optimizer arg not valid!")
+    exit(1)
 
-# =============================================================================
-# Design variable setup
-# =============================================================================
-def regModel(val, DASolver):
-    for idxI in range(len(val)):
-        val1 = float(val[idxI])
-        DASolver.setRegressionParameter("model1", idxI, val1)
+prob.driver.options["debug_print"] = ["nl_cons", "objs", "desvars"]
+prob.driver.options["print_opt_prob"] = True
+prob.driver.hist_file = "OptView.hst"
 
-
-# =============================================================================
-# DAFoam initialization
-# =============================================================================
-DASolver = PYDAFOAM(options=daOptions, comm=gcomm)
-nParameters = DASolver.getNRegressionParameters("model1")
-if args.readDV == 1:
-    with open("./designVariable.json") as f:
-        dv = json.load(f)
-    parameter0 = dv["parameter"]
-else:
-    # parameter0 = np.ones(nParameters) * 0.01
-    parameter0 = (np.random.rand(nParameters) - 0.5) * 0.05
-DASolver.addInternalDV("parameter", parameter0, regModel, lower=-10, upper=10, scale=1.0)
-evalFuncs = []
-DASolver.setEvalFuncs(evalFuncs)
-
-# =============================================================================
-# Initialize optFuncs for optimization
-# =============================================================================
-optFuncs.DASolver = DASolver
-optFuncs.DVGeo = None
-optFuncs.DVCon = None
-optFuncs.evalFuncs = evalFuncs
-optFuncs.gcomm = gcomm
-
-# =============================================================================
-# Task
-# =============================================================================
-if args.task == "opt":
-
-    optProb = Optimization("opt", objFun=optFuncs.calcObjFuncValues, comm=gcomm)
-    DASolver.addVariablesPyOpt(optProb)
-
-    optProb.addObj("VAR", scale=1)
-
-    if gcomm.rank == 0:
-        print(optProb)
-
-    DASolver.runColoring()
-
-    opt = OPT(args.optimizer, options=optOptions)
-    histFile = "./%s_hist.hst" % args.optimizer
-    sol = opt(optProb, sens=optFuncs.calcObjFuncSens, storeHistory=histFile)
-    if gcomm.rank == 0:
-        print(sol)
-
-elif args.task == "runPrimal":
-
-    optFuncs.runPrimal()
-
-elif args.task == "runAdjoint":
-
-    optFuncs.runAdjoint()
-
-elif args.task == "runForwardAD":
-
-    optFuncs.runForwardAD("parameter", args.seedIndex)
-
+if args.task == "run_driver":
+    # run the optimization
+    prob.run_driver()
+elif args.task == "run_model":
+    # just run the primal once
+    prob.run_model()
+elif args.task == "compute_totals":
+    # just run the primal and adjoint once
+    prob.run_model()
+    totals = prob.compute_totals()
+    if MPI.COMM_WORLD.rank == 0:
+        print(totals)
+elif args.task == "check_totals":
+    # verify the total derivatives against the finite-difference
+    prob.run_model()
+    prob.check_totals(compact_print=False, step=1e-3, form="central", step_calc="abs")
 else:
     print("task arg not found!")
-    exit(0)
+    exit(1)
