6a9fdc0c7eb61147a17c1bad98e161758ef7299b
Ping He <phe@iastate.edu>
2025-03-04 13:10:05 -0600

    Changed the NREL6 script

---
+++ NREL6_Wind_Turbine/runScript.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+import os
+import argparse
+import numpy as np
+from mpi4py import MPI
+import openmdao.api as om
+from mphys.multipoint import Multipoint
+from dafoam.mphys import DAFoamBuilder, OptFuncs
+from mphys.scenario_aerodynamic import ScenarioAerodynamic
+from pygeo.mphys import OM_DVGEOCOMP
+from pygeo import geo_utils
+
+parser = argparse.ArgumentParser()
+# which optimizer to use. Options are: IPOPT (default), SLSQP, and SNOPT
+parser.add_argument("-optimizer", help="optimizer to use", type=str, default="IPOPT")
+# which task to run. Options are: run_driver (default), run_model, compute_totals, check_totals
+parser.add_argument("-task", help="type of run to do", type=str, default="run_driver")
+args = parser.parse_args()
+
+# =============================================================================
+# Input Parameters
+# =============================================================================
+
+daOptions = {
+    "solverName": "DATurboFoam",
+    "designSurfaces": ["blade"],
+    "primalMinResTolDiff": 1e4,
+    "primalMinResTol": 1e-9,
+    "objFunc": {
+        "CMX": {
+            "type": "moment",
+            "source": "patchToFace",
+            "patches": ["blade"],
+            "axis": [1.0, 0.0, 0.0],
+            "center": [0.0, 0.0, 0.0],
+            "scale": 1.0,
+        },
+    },
+    "adjStateOrdering": "cell",
+    "normalizeStates": {"U": 10.0, "p": 100000.0, "nuTilda": 1e-3, "phi": 1.0, "T": 300.0},
+    "adjEqnOption": {"gmresRelTol": 1.0e-5, "pcFillLevel": 1, "jacMatReOrdering": "natural"},
+    "adjPCLag": 5,
+    "transonicPCOption": 1,
+    "checkMeshThreshold": {"maxNonOrth": 70.0, "maxSkewness": 6.0, "maxAspectRatio": 1000.0},
+    "inputInfo": {
+        "aero_vol_coords": {"type": "volCoord", "components": ["solver", "function"]},
+    },
+}
+
+meshOptions = {
+    "gridFile": os.getcwd(),
+    "fileType": "OpenFOAM",
+    # point and normal for the symmetry plane
+    "symmetryPlanes": [],
+}
+
+
+# Top class to setup the optimization problem
+class Top(Multipoint):
+    def setup(self):
+
+        # create the builder to initialize the DASolvers
+        dafoam_builder = DAFoamBuilder(daOptions, meshOptions, scenario="aerodynamic")
+        dafoam_builder.initialize(self.comm)
+
+        # add the design variable component to keep the top level design variables
+        self.add_subsystem("dvs", om.IndepVarComp(), promotes=["*"])
+
+        # add the mesh component
+        self.add_subsystem("mesh", dafoam_builder.get_mesh_coordinate_subsystem())
+
+        # add the geometry component (FFD)
+        self.add_subsystem("geometry", OM_DVGEOCOMP(file="FFD/wingFFD.xyz", type="ffd"))
+
+        # add a scenario (flow condition) for optimization, we pass the builder
+        # to the scenario to actually run the flow and adjoint
+        self.mphys_add_scenario("scenario1", ScenarioAerodynamic(aero_builder=dafoam_builder))
+
+        # need to manually connect the x_aero0 between the mesh and geometry components
+        # here x_aero0 means the surface coordinates of structurally undeformed mesh
+        self.connect("mesh.x_aero0", "geometry.x_aero_in")
+        # need to manually connect the x_aero0 between the geometry component and the scenario1
+        # scenario group
+        self.connect("geometry.x_aero0", "scenario1.x_aero")
+
+    def configure(self):
+
+        # get the surface coordinates from the mesh component
+        points = self.mesh.mphys_get_surface_mesh()
+
+        # add pointset to the geometry component
+        self.geometry.nom_add_discipline_coords("aero", points)
+
+        # use the shape function to define symmetry shapes for two blades
+        pts0 = self.geometry.DVGeo.getLocalIndex(0)
+        pts1 = self.geometry.DVGeo.getLocalIndex(1)
+        dir_x = np.array([1.0, 0.0, 0.0])
+        shapes = []
+        for i in range(pts0.shape[0]):
+            for j in range(pts0.shape[1]):
+                for k in range(pts0.shape[2]):
+                    # k=0 and k=1 move together to ensure symmetry
+                    shapes.append({pts0[i, j, k]: dir_x, pts1[i, j, k]: dir_x})
+        self.geometry.nom_addShapeFunctionDV(dvName="shape", shapes=shapes)
+
+        # setup the volume and thickness constraints
+        # add the design variables to the dvs component's output
+        self.dvs.add_output("shape", val=np.array([0] * len(shapes)))
+        # manually connect the dvs output to the geometry and scenario1
+        self.connect("shape", "geometry.shape")
+
+        # define the design variables
+        self.add_design_var("shape", lower=-0.5, upper=0.5, scaler=10.0)
+
+        # add objective and constraints to the top level
+        self.add_objective("scenario1.aero_post.CMX", scaler=-1.0)
+
+
+# OpenMDAO setup
+prob = om.Problem()
+
+prob.model = Top()
+prob.setup(mode="rev")
+om.n2(prob, show_browser=False, outfile="mphys.html")
+
+# initialize the optimization function
+optFuncs = OptFuncs(daOptions, prob)
+
+# use pyoptsparse to setup optimization
+prob.driver = om.pyOptSparseDriver()
+prob.driver.options["optimizer"] = args.optimizer
+# options for optimizers
+if args.optimizer == "SNOPT":
+    prob.driver.opt_settings = {
+        "Major feasibility tolerance": 1.0e-5,
+        "Major optimality tolerance": 1.0e-5,
+        "Minor feasibility tolerance": 1.0e-5,
+        "Verify level": -1,
+        "Function precision": 1.0e-5,
+        "Major iterations limit": 100,
+        "Nonderivative linesearch": None,
+        "Print file": "opt_SNOPT_print.txt",
+        "Summary file": "opt_SNOPT_summary.txt",
+    }
+elif args.optimizer == "IPOPT":
+    prob.driver.opt_settings = {
+        "tol": 1.0e-5,
+        "constr_viol_tol": 1.0e-5,
+        "max_iter": 100,
+        "print_level": 5,
+        "output_file": "opt_IPOPT.txt",
+        "mu_strategy": "adaptive",
+        "limited_memory_max_history": 10,
+        "nlp_scaling_method": "none",
+        "alpha_for_y": "full",
+        "recalc_y": "yes",
+    }
+elif args.optimizer == "SLSQP":
+    prob.driver.opt_settings = {
+        "ACC": 1.0e-5,
+        "MAXIT": 100,
+        "IFILE": "opt_SLSQP.txt",
+    }
+else:
+    print("optimizer arg not valid!")
+    exit(1)
+
+prob.driver.options["debug_print"] = ["nl_cons", "objs", "desvars"]
+prob.driver.options["print_opt_prob"] = True
+prob.driver.hist_file = "OptView.hst"
+
+
+if args.task == "run_driver":
+    # run the optimization
+    prob.run_driver()
+elif args.task == "run_model":
+    # just run the primal once
+    prob.run_model()
+elif args.task == "compute_totals":
+    # just run the primal and adjoint once
+    prob.run_model()
+    totals = prob.compute_totals()
+    if MPI.COMM_WORLD.rank == 0:
+        print(totals)
+elif args.task == "check_totals":
+    # verify the total derivatives against the finite-difference
+    prob.run_model()
+    prob.check_totals(compact_print=False, step=1e-3, form="central", step_calc="abs")
+else:
+    print("task arg not found!")
+    exit(1)
