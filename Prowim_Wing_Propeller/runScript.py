d33ef488ceebecb779b638e6e5632e8aca9b339e
Ping He <phe@iastate.edu>
2025-03-05 22:20:51 -0600

    Updated the prowim case.

---
+++ Prowim_Wing_Propeller/runScript.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python
+import os
+import argparse
+import numpy as np
+from mpi4py import MPI
+import openmdao.api as om
+from mphys.multipoint import Multipoint
+from dafoam.mphys import DAFoamBuilder, OptFuncs
+from mphys.scenario_aerodynamic import ScenarioAerodynamic
+from pygeo.mphys import OM_DVGEOCOMP
+from pygeo import geo_utils
+
+parser = argparse.ArgumentParser()
+# which optimizer to use. Options are: IPOPT (default), SLSQP, and SNOPT
+parser.add_argument("-optimizer", help="optimizer to use", type=str, default="IPOPT")
+# which task to run. Options are: run_driver (default), run_model, compute_totals, check_totals
+parser.add_argument("-task", help="type of run to do", type=str, default="run_driver")
+args = parser.parse_args()
+
+# =============================================================================
+# Input Parameters
+# =============================================================================
+
+# global parameters
+U0 = 100.0
+p0 = 101325.0
+nuTilda0 = 4.5e-5
+T0 = 300.0
+CL_target = 0.5
+aoa0 = 3.0
+A0 = 0.1536
+rho0 = p0 / T0 / 287.0
+
+
+daOptions = {
+    "designSurfaces": ["wing"],
+    "solverName": "DARhoSimpleFoam",
+    "primalMinResTol": 1.0e-8,
+    "primalBC": {
+        "U0": {"variable": "U", "patches": ["inout"], "value": [U0, 0.0, 0.0]},
+        "p0": {"variable": "p", "patches": ["inout"], "value": [p0]},
+        "T0": {"variable": "T", "patches": ["inout"], "value": [T0]},
+        "nuTilda0": {"variable": "nuTilda", "patches": ["inout"], "value": [nuTilda0]},
+        "useWallFunction": True,
+    },
+    "function": {
+        "CD": {
+            "type": "force",
+            "source": "patchToFace",
+            "patches": ["wing"],
+            "directionMode": "parallelToFlow",
+            "patchVelocityInputName": "patchV",
+            "scale": 1.0 / (0.5 * U0 * U0 * A0 * rho0),
+        },
+        "CL": {
+            "type": "force",
+            "source": "patchToFace",
+            "patches": ["wing"],
+            "directionMode": "normalToFlow",
+            "patchVelocityInputName": "patchV",
+            "scale": 1.0 / (0.5 * U0 * U0 * A0 * rho0),
+        },
+    },
+    "fvSource": {
+        "disk1": {
+            "type": "actuatorDisk",
+            "source": "cylinderAnnulusSmooth",
+            "center": [-0.2, 0.0, 0.3],
+            "direction": [1.0, 0.0, 0.0],
+            "innerRadius": 0.01,
+            "outerRadius": 0.12,
+            "rotDir": "left",
+            "scale": 30000.0,
+            "POD": 0.85,
+            "eps": 0.04,  # eps should be of cell size
+            "expM": 1.0,
+            "expN": 0.5,
+            "adjustThrust": 1,
+            "targetThrust": 55.0,
+        },
+    },
+    "adjEqnOption": {
+        "gmresRelTol": 1.0e-6,
+        "pcFillLevel": 1,
+        "jacMatReOrdering": "rcm",
+        "gmresMaxIters": 1000,
+        "gmresRestart": 1000,
+    },
+    "normalizeStates": {
+        "U": U0,
+        "p": p0,
+        "T": T0,
+        "nuTilda": 1e-3,
+        "phi": 1.0,
+    },
+    "inputInfo": {
+        "aero_vol_coords": {"type": "volCoord", "components": ["solver", "function"]},
+        "patchV": {
+            "type": "patchVelocity",
+            "patches": ["inout"],
+            "flowAxis": "x",
+            "normalAxis": "y",
+            "components": ["solver", "function"],
+        },
+        "actuator_disk": {
+            "type": "fvSourcePar",
+            "fvSourceName": "disk1",
+            "indices": [2],  # z location
+            "components": ["solver", "function"],
+        },
+    },
+}
+
+# Mesh deformation setup
+meshOptions = {
+    "gridFile": os.getcwd(),
+    "fileType": "OpenFOAM",
+    # point and normal for the symmetry plane
+    "symmetryPlanes": [[[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]]],
+}
+
+
+# Top class to setup the optimization problem
+class Top(Multipoint):
+    def setup(self):
+
+        # create the builder to initialize the DASolvers
+        dafoam_builder = DAFoamBuilder(daOptions, meshOptions, scenario="aerodynamic")
+        dafoam_builder.initialize(self.comm)
+
+        # add the design variable component to keep the top level design variables
+        self.add_subsystem("dvs", om.IndepVarComp(), promotes=["*"])
+
+        # add the mesh component
+        self.add_subsystem("mesh", dafoam_builder.get_mesh_coordinate_subsystem())
+
+        # add the geometry component (FFD)
+        self.add_subsystem("geometry", OM_DVGEOCOMP(file="FFD/wingFFD.xyz", type="ffd"))
+
+        # add a scenario (flow condition) for optimization, we pass the builder
+        # to the scenario to actually run the flow and adjoint
+        self.mphys_add_scenario("scenario1", ScenarioAerodynamic(aero_builder=dafoam_builder))
+
+        # need to manually connect the x_aero0 between the mesh and geometry components
+        # here x_aero0 means the surface coordinates of structurally undeformed mesh
+        self.connect("mesh.x_aero0", "geometry.x_aero_in")
+        # need to manually connect the x_aero0 between the geometry component and the scenario1
+        # scenario group
+        self.connect("geometry.x_aero0", "scenario1.x_aero")
+
+    def configure(self):
+
+        # get the surface coordinates from the mesh component
+        points = self.mesh.mphys_get_surface_mesh()
+
+        # add pointset to the geometry component
+        self.geometry.nom_add_discipline_coords("aero", points)
+
+        # set the triangular points to the geometry component for geometric constraints
+        tri_points = self.mesh.mphys_get_triangulated_surface()
+        self.geometry.nom_setConstraintSurface(tri_points)
+
+        # Create reference axis for the twist variable
+        nRefAxPts = self.geometry.nom_addRefAxis(name="wingAxis", xFraction=0.25, alignIndex="k")
+
+        # Set up global design variables. We dont change the root twist
+        def twist(val, geo):
+            for i in range(1, nRefAxPts):
+                geo.rot_z["wingAxis"].coef[i] = -val[i - 1]
+
+        # add twist variable
+        self.geometry.nom_addGlobalDV(dvName="twist", value=np.array([0] * (nRefAxPts - 1)), func=twist)
+
+        # select the FFD points to move
+        pts = self.geometry.DVGeo.getLocalIndex(0)
+        indexList = pts[:, :, :].flatten()
+        PS = geo_utils.PointSelect("list", indexList)
+        nShapes = self.geometry.nom_addLocalDV(dvName="shape", pointSelect=PS)
+
+        # setup the volume and thickness constraints
+        leList = [[0.01, 0.0, 1e-3], [0.01, 0.0, 0.63]]
+        teList = [[0.235, 0.0, 1e-3], [0.235, 0.0, 0.63]]
+        self.geometry.nom_addThicknessConstraints2D("thickcon", leList, teList, nSpan=10, nChord=10)
+        self.geometry.nom_addVolumeConstraint("volcon", leList, teList, nSpan=10, nChord=10)
+        # add the LE/TE constraints
+        self.geometry.nom_add_LETEConstraint("lecon", volID=0, faceID="iLow")
+        self.geometry.nom_add_LETEConstraint("tecon", volID=0, faceID="iHigh")
+        # LE radius constraint
+        self.geometry.nom_addLERadiusConstraints("radius", leList, nSpan=10, axis=[0, 1, 0], chordDir=[-1, 0, 0])
+        # wing surface spanwise curvature constraint
+        self.geometry.nom_addCurvatureConstraint1D(
+            "curvature1",
+            start=[0.12, 0, 0.02],
+            end=[0.12, 0, 0.6],
+            nPts=20,
+            axis=[0, 1, 0],
+            curvatureType="mean",
+            scaled=False,
+        )
+
+        # add the design variables to the dvs component's output
+        self.dvs.add_output("twist", val=np.array([0] * (nRefAxPts - 1)))
+        self.dvs.add_output("shape", val=np.array([0] * nShapes))
+        self.dvs.add_output("patchV", val=np.array([U0, aoa0]))
+        self.dvs.add_output("actuator_disk", val=np.array([0.3]))
+        # manually connect the dvs output to the geometry and scenario1
+        self.connect("twist", "geometry.twist")
+        self.connect("shape", "geometry.shape")
+        self.connect("patchV", "scenario1.patchV")
+        self.connect("actuator_disk", "scenario1.actuator_disk")
+
+        # define the design variables
+        self.add_design_var("twist", lower=-10.0, upper=10.0, scaler=0.1)
+        self.add_design_var("shape", lower=-1.0, upper=1.0, scaler=10.0)
+        self.add_design_var("patchV", lower=[U0, 0.0], upper=[U0, 10.0], scaler=0.1)
+        self.add_design_var("actuator_disk", lower=0.2, upper=0.6, scaler=1.0)
+
+        # add objective and constraints to the top level
+        self.add_objective("scenario1.aero_post.CD", scaler=1.0)
+        self.add_constraint("scenario1.aero_post.CL", equals=CL_target, scaler=1.0)
+        self.add_constraint("geometry.thickcon", lower=0.5, upper=3.0, scaler=1.0)
+        self.add_constraint("geometry.volcon", lower=1.0, scaler=1.0)
+        self.add_constraint("geometry.tecon", equals=0.0, scaler=1.0, linear=True)
+        self.add_constraint("geometry.lecon", equals=0.0, scaler=1.0, linear=True)
+        self.add_constraint("geometry.radius", lower=1.0, upper=3.0, scaler=1.0)
+        self.add_constraint("geometry.curvature1", lower=0.0, upper=0.1, scaler=1.0)
+
+
+# OpenMDAO setup
+prob = om.Problem()
+prob.model = Top()
+prob.setup(mode="rev")
+om.n2(prob, show_browser=False, outfile="mphys.html")
+
+# initialize the optimization function
+optFuncs = OptFuncs(daOptions, prob)
+
+# use pyoptsparse to setup optimization
+prob.driver = om.pyOptSparseDriver()
+prob.driver.options["optimizer"] = args.optimizer
+# options for optimizers
+if args.optimizer == "SNOPT":
+    prob.driver.opt_settings = {
+        "Major feasibility tolerance": 1.0e-5,
+        "Major optimality tolerance": 1.0e-5,
+        "Minor feasibility tolerance": 1.0e-5,
+        "Verify level": -1,
+        "Function precision": 1.0e-5,
+        "Major iterations limit": 100,
+        "Nonderivative linesearch": None,
+        "Print file": "opt_SNOPT_print.txt",
+        "Summary file": "opt_SNOPT_summary.txt",
+    }
+elif args.optimizer == "IPOPT":
+    prob.driver.opt_settings = {
+        "tol": 1.0e-5,
+        "constr_viol_tol": 1.0e-5,
+        "max_iter": 100,
+        "print_level": 5,
+        "output_file": "opt_IPOPT.txt",
+        "mu_strategy": "adaptive",
+        "limited_memory_max_history": 10,
+        "nlp_scaling_method": "none",
+        "alpha_for_y": "full",
+        "recalc_y": "yes",
+    }
+elif args.optimizer == "SLSQP":
+    prob.driver.opt_settings = {
+        "ACC": 1.0e-5,
+        "MAXIT": 100,
+        "IFILE": "opt_SLSQP.txt",
+    }
+else:
+    print("optimizer arg not valid!")
+    exit(1)
+
+prob.driver.options["debug_print"] = ["nl_cons", "objs", "desvars"]
+prob.driver.options["print_opt_prob"] = True
+prob.driver.hist_file = "OptView.hst"
+
+
+if args.task == "run_driver":
+    # solve CL
+    optFuncs.findFeasibleDesign(["scenario1.aero_post.CL"], ["patchV"], targets=[CL_target], designVarsComp=[1])
+    # run the optimization
+    prob.run_driver()
+elif args.task == "run_model":
+    # just run the primal once
+    prob.run_model()
+elif args.task == "compute_totals":
+    # just run the primal and adjoint once
+    prob.run_model()
+    totals = prob.compute_totals()
+    if MPI.COMM_WORLD.rank == 0:
+        print(totals)
+elif args.task == "check_totals":
+    # verify the total derivatives against the finite-difference
+    prob.run_model()
+    prob.check_totals(compact_print=False, step=1e-3, form="central", step_calc="abs")
+else:
+    print("task arg not found!")
+    exit(1)
