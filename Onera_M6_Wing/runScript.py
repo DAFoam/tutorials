f83b1126cc067ca4b93567e037f787ec519a5de0
Ping He <phe@iastate.edu>
2025-03-21 16:06:18 -0500

    Updated M6.

--- Onera_M6_Wing/runScript.py
+++ Onera_M6_Wing/runScript.py
@@ -63,7 +63,7 @@ daOptions = {
         },
     },
     "adjStateOrdering": "cell",
-    "adjEqnOption": {"gmresRelTol": 1.0e-5, "pcFillLevel": 1, "jacMatReOrdering": "natural"},
+    "adjEqnOption": {"gmresRelTol": 1.0e-4, "pcFillLevel": 1, "jacMatReOrdering": "natural"},
     # transonic preconditioner to speed up the adjoint convergence
     "transonicPCOption": 2,
     "normalizeStates": {"U": U0, "p": p0, "nuTilda": nuTilda0 * 10.0, "phi": 1.0, "T": T0},
