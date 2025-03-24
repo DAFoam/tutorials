1b604620037ce01b562a3ce6b35046f08c5067a9
Ping He <phe@iastate.edu>
2025-03-24 10:14:33 -0500

    Updated ramp unsteady.g

--- Ramp/unsteady/train/preProcessing.sh
+++ Ramp/unsteady/train/preProcessing.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 
-# the script will exit if there is any error
-set -e
-
 if [ -z "$WM_PROJECT" ]; then
   echo "OpenFOAM environment not found, forgot to source the OpenFOAM bashrc?"
   exit 1
 fi
-
-# for the remove command
-shopt -s extglob 
 
 # spin up the flow for 0.1 s
 # spin up SST
 cp constant/turbulenceProperties_SST constant/turbulenceProperties
 cp system/controlDict_spinup system/controlDict
 rm -rf 0
-cp -r 0_orig 0
+cp -rf 0_orig 0
 mpirun -np 2 python runPrimal.py
 reconstructPar 
 rm -rf 0.1/polyMesh 0.1/uniform
@@ -27,7 +21,7 @@ rm -rf processor*
 cp constant/turbulenceProperties_SA constant/turbulenceProperties
 cp system/controlDict_spinup system/controlDict
 rm -rf 0
-cp -r 0_orig 0
+cp -rf 0_orig 0
 mpirun -np 2 python runPrimal.py
 reconstructPar 
 rm -rf 0.1/polyMesh 0.1/uniform
@@ -39,7 +33,7 @@ rm -rf processor*
 cp constant/turbulenceProperties_SST constant/turbulenceProperties
 cp system/controlDict_full system/controlDict
 rm -rf 0
-cp -r 0_SST 0
+cp -rf 0_SST 0
 mpirun -np 2 python runPrimal.py
 reconstructPar 
 rm -rf processor*
@@ -48,14 +42,13 @@ rm -rf processor*
 getFIData -refFieldName U -refFieldType vector
 getFIData -refFieldName p -refFieldType scalar
 
-rm -rf 0.*/!(*Data.gz) 1/!(*Data.gz) 1.*/!(*Data.gz) 
 rm -rf 0
-cp -r 0_SA 0
+cp -rf 0_SA 0
 
 # decompose all times
-decomposePar -time '0:'
+#decomposePar -time '0:'
 
-rm -rf 0.* 1 1.*
+#rm -rf 0.* 1 1.*
 
 # get ready for the SA run
 cp constant/turbulenceProperties_SA constant/turbulenceProperties
