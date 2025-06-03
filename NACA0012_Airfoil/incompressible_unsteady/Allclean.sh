9ac7486f02c77fdba65815221eec3f78d370c754
Ping He <phe@iastate.edu>
2025-06-03 09:47:47 -0500

    Added a coupled steady and unsteady case.

--- NACA0012_Airfoil/incompressible_unsteady/Allclean.sh
+++ NACA0012_Airfoil/incompressible_unsteady/Allclean.sh
@@ -7,6 +7,7 @@ do
         [Yy]* ) 
             # clean everyting
             echo "Cleaning..."
+            cd cruise
             rm -rf 0
             rm -rf postProcessing
             rm -rf constant/extendedFeatureEdgeMesh
@@ -15,6 +16,16 @@ do
             rm -rf *.bin *.info *.dat *.xyz *.stl
             rm -rf processor* 0.0000*
             rm -rf {1..9}*
+            cd ../maxLift
+            rm -rf 0
+            rm -rf postProcessing
+            rm -rf constant/extendedFeatureEdgeMesh
+            rm -rf constant/triSurface
+            rm -rf constant/polyMesh/
+            rm -rf *.bin *.info *.dat *.xyz *.stl
+            rm -rf processor* 0.0000*
+            rm -rf {1..9}*
+            cd ../
             exit
             ;;
         [Nn]* ) exit;;
