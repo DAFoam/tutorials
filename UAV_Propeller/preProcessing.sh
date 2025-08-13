963ed79827db2afbf167f76fd065e36037363a62
szoppelt <145373267+szoppelt@users.noreply.github.com>
2025-08-13 11:35:39 -0500

    Update preProcessing.sh

--- UAV_Propeller/preProcessing.sh
+++ UAV_Propeller/preProcessing.sh
@@ -8,19 +8,19 @@ fi
 # pre-processing
 
 # get the geometry
-if [ -f "mdolab_wing_surface_mesh.cgns.tar.gz" ]; then
-  echo "Surface geometry mdolab_wing_surface_mesh.cgns.tar.gz already exists."
+if [ -f "mdolab_UAV_prop_CFDmesh.cgns.tar.gz" ]; then
+  echo "Surface geometry mdolab_UAV_prop_CFDmesh.cgns.tar.gz already exists."
 else
   echo "Downloading surface geometry mdolab_wing_surface_mesh.cgns.tar.gz"
-  wget https://github.com/dafoam/files/releases/download/v1.0.0/mdolab_wing_surface_mesh.cgns.tar.gz
+  wget https://github.com/dafoam/files/releases/download/v1.0.0/mdolab_UAV_prop_CFDmesh.cgns.tar.gz
 fi
-tar -xvf mdolab_wing_surface_mesh.cgns.tar.gz
+tar -xvf mdolab_UAV_prop_CFDmesh.cgns.tar.gz
 
 # generate mesh
 echo "Generating mesh.."
 
 # coarsen the surface mesh three times
-cgns_utils coarsen mdolab_wing_surface_mesh.cgns surfaceMesh.cgns
+cgns_utils coarsen mdolab_UAV_prop_CFDmesh.cgns surfaceMesh.cgns
 #cgns_utils coarsen surfaceMesh.cgns
 python genWingMesh.py &> logMeshGeneration.txt
 plot3dToFoam -noBlank volumeMesh.xyz >> logMeshGeneration.txt
