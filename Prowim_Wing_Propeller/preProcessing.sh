b13a23446ba191638e0b988e1e7ea983e4455614
heyecanutke <86077517+heyecanutke@users.noreply.github.com>
2025-08-14 01:26:33 -0500

    Update preProcessing.sh

--- Prowim_Wing_Propeller/preProcessing.sh
+++ Prowim_Wing_Propeller/preProcessing.sh
@@ -6,23 +6,14 @@ if [ -z "$WM_PROJECT" ]; then
 fi
 
 # pre-processing
+tar -xvf triSurface.tar.gz
+mv triSurface constant/
 
-if [ -f "prowim_wing_surface_mesh.cgns.tar.gz" ]; then
-  echo "Surface mesh prowim_wing_surface_mesh.cgns.tar.gz already exists."
-else
-  echo "Downloading surface mesh prowim_wing_surface_mesh.cgns.tar.gz"
-  wget https://github.com/dafoam/files/releases/download/v1.0.0/prowim_wing_surface_mesh.cgns.tar.gz
-fi
-rm prowim_wing_surface_mesh.cgns
-tar -xvf prowim_wing_surface_mesh.cgns.tar.gz
-# coarsen the surface mesh three times
-cgns_utils coarsen prowim_wing_surface_mesh.cgns surfaceMesh.cgns
-cgns_utils coarsen surfaceMesh.cgns
 # generate mesh
 echo "Generating mesh.."
-python genWingMesh.py &> logMeshGeneration.txt
-plot3dToFoam -noBlank volumeMesh.xyz >> logMeshGeneration.txt
-autoPatch 60 -overwrite >> logMeshGeneration.txt
+blockMesh &> logMeshGeneration.txt
+surfaceFeatureExtract >> logMeshGeneration.txt
+snappyHexMesh -overwrite >> logMeshGeneration.txt
 createPatch -overwrite >> logMeshGeneration.txt
 renumberMesh -overwrite >> logMeshGeneration.txt
 echo "Generating mesh.. Done!"
