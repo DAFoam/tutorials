2dff5315c1d98bfe9d77071893cb0033ef0a5c70
Chris Psenica <122938605+ChrisPsenica@users.noreply.github.com>
2026-01-24 17:51:42 -0600

    Adding in the SBO optimization runScript.
    This is the SBO runScript.py associated with the NACA0012 incompressible optimization case.

--- NACA0012_Airfoil/incompressible/runScript_SBO.py
+++ NACA0012_Airfoil/incompressible/runScript_SBO.py
@@ -1,27 +1,39 @@
+
 import numpy as np
 from runScript import prob as NACA0012
 from dafoam.pyDAFoam import surrogateOptimization
 
-xlimits = np.array([[-0.01, 0.01]] * 8)
-xlimits = np.array([[-0.05, 0.05]] * 10)        #~ DV bounds
-xlimits[-1] = [0 , 3]                           #~ last DV is AOA which has different bounds, so here we adjust the bounds
-xlimits[-2] = [10 , 10 + 1e-9]                  #~ restrict far field velocity to 10m/s
-xlimits[-3] = [-0.01 , 0.01]                    #~ restrict FFD points near TE of airfoil
-xlimits[-4] = [-0.01 , 0.01]
+
+# define design variables (name & size)
+dvNames = ["shape", "patchV"]
+dvSizes = [8, 2]
+
+# prescribe bounds on design variables
+xlimits = np.array([[0.0, 0.05]] * 10)
+
+# adjust bounds for AOA, fix velocity to 10m/s
+xlimits[-1] = [1, 1.5]                          
+xlimits[-2] = [10, 10 + 1e-9]                  
+
+objFunc = 'scenario1.aero_post.CD'
+cons = ['scenario1.aero_post.CL', 'geometry.thickcon', 'geometry.volcon', 'geometry.rcon']
+conWeights = [1e10, 10, 10, 1e2]
+consEqs = ["x - 0.5", "0.5 <= x <= 3", "1 <= x", "0.8 <= x"]
 
 surrogateOptions = {
-    "optType"    : "constrained",               #~ type of optimization problem (constrained or unconstrained)     
-    "criterion"  : "EI",                        #~ criterion for next evaluation point determination -> EGO algorithm
-    "iters"      : 20,                          #~ num iterations to optimize function
-    "numDOE"     : 10,                          #~ number of sampling points
-    "seed"       : 42,                          #~ seed value to reproduce results
-    "dvNames"    : ["shape" , "patchV"],        #~ names of design variables
-    "dvSizes"    : [8 , 2],                     #~ number of points for each design variable
-    "dvBounds"   : xlimits,                     #~ design variable bounds            
-    "objFunc"    : 'scenario1.aero_post.CD',    #~ objective function
-    "cons"       : ['scenario1.aero_post.CL'],  #~ quantity to constrain
-    "conWeights" : [10],                        #~ constraint weight
-    "consEqs"    : ["x - 0.5"],                 #~ constraint equation(s)
+    "criterion"  : "EI",           # criterion for next evaluation point
+    "iters"      : 10,             # num iterations to optimize function
+    "numDOE"     : 15,             # number of sampling points
+    "seed"       : 43,             # seed value to reproduce results
+    "dvNames"    : dvNames,        # names of design variables
+    "dvSizes"    : dvSizes,        # number of points for each design variable
+    "dvBounds"   : xlimits,        # design variable bounds
+    "objFunc"    : objFunc,        # objective function
+    "cons"       : cons,           # quantity to constrain
+    "conWeights" : conWeights,     # constraint weight
+    "consEqs"    : consEqs,        # constraint equation(s)
+    "surrogate"  : "MGP",          # which surrogate model to use (Kriging-based)
 }
 
-surrogateOptimization(surrogateOptions , NACA0012)
+surrogateOptimization(surrogateOptions, NACA0012)  # pass surrogateOptions and OpenMDAO model to surrogateOptimization class
+
